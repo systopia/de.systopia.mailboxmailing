d33519443ec7d93efa7f3c7b6da561ad35b61fb1
Jens Schuppe <schuppe@systopia.de>
2019-02-11 16:10:39 +0100

    Civix Update and credit comment

--- mailboxmailing.php
+++ mailboxmailing.php
@@ -1,4 +1,17 @@
 <?php
+/*-------------------------------------------------------+
+| SYSTOPIA Malbox Mailing Extension                      |
+| Copyright (C) 2019 SYSTOPIA                            |
+| Author: J. Schuppe (schuppe@systopia.de)               |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
 
 require_once 'mailboxmailing.civix.php';
 use CRM_Mailboxmailing_ExtensionUtil as E;
@@ -121,6 +134,17 @@ function mailboxmailing_civicrm_angularModules(&$angularModules) {
  */
 function mailboxmailing_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   _mailboxmailing_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
+ */
+function mailboxmailing_civicrm_entityTypes(&$entityTypes) {
+  _mailboxmailing_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
